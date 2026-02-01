ba39c2294e631a2bbc8a060538cfe86eba69eec1
ivanhromovyi <ivanh@uia.no>
2026-02-01 18:49:56 +0100

    implement makeAbbr() function

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -1,6 +1,13 @@
 package mate.academy
 
 fun makeAbbr(source: String) : String {
-    // implement this function
-    return ""
+    var result = ""
+    if (!source.isEmpty()) {
+        result += source[0].uppercase()
+    }
+    for (i in 0 until source.length - 1) {
+        if (source[i] == ' ')
+        result += source[i + 1].uppercase()
+    }
+    return result
 }
